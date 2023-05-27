c2bd063ce44ff9569a9115860bedf98169a79270
Koukou3000 <1968204044@qq.com>
2023-05-27 20:14:02 +0800

    vue-cli-stepup

---
+++ cli_test/src/App.vue
@@ -0,0 +1,26 @@
+<!--汇总如Student.vue的组件进行展示-->
+<template>
+    <div>
+        <HelloStudent></HelloStudent>
+    </div>
+</template>
+
+<script>
+
+    import HelloStudent from './components/HelloStudent'
+    console.log('Student.vue is read!')
+    
+    const App = Vue.extend({
+        name:'App',
+        components:{HelloStudent},
+     
+    })
+    export default App
+//     export default{
+//         name:'App',
+//         components:{HelloStudent}
+//    }
+</script>
+
+
+
